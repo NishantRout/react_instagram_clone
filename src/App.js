9c26ebf9ce40712e5acfc9913cc231bfc1918cfb
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-10-03 23:28:50 +0530

    Header UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      {/* Header */}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
